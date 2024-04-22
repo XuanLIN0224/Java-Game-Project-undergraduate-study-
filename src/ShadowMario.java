50f4704bd47217e032d542ba3a8bb847a3cf4780
xulin2 <xuan.lin1@student.unimelb.edu.au>
2024-04-22 19:33:01 +1000

    Initial commit

---
+++ src/ShadowMario.java
@@ -0,0 +1,51 @@
+import bagel.*;
+import java.util.Properties;
+
+/**
+ * Skeleton Code for SWEN20003 Project 2, Semester 1, 2024
+ *
+ * Please enter your name below
+ * @author
+ */
+public class ShadowMario extends AbstractGame {
+
+    private final Image BACKGROUND_IMAGE;
+
+    /**
+     * The constructor
+     */
+    public ShadowMario(Properties game_props, Properties message_props) {
+        super(Integer.parseInt(game_props.getProperty("windowWidth")),
+              Integer.parseInt(game_props.getProperty("windowHeight")),
+              message_props.getProperty("title"));
+
+        BACKGROUND_IMAGE = new Image(game_props.getProperty("backgroundImage"));
+    }
+
+    /**
+     * The entry point for the program.
+     */
+    public static void main(String[] args) {
+        Properties game_props = IOUtils.readPropertiesFile("res/app.properties");
+        Properties message_props = IOUtils.readPropertiesFile("res/message_en.properties");
+        ShadowMario game = new ShadowMario(game_props, message_props);
+        game.run();
+    }
+
+    /**
+     * Performs a state update of the selected level.
+     * Allows the game to exit when the escape key is pressed.
+     * Handle screen navigation between levels and instruction pages here.
+     */
+    @Override
+    protected void update(Input input) {
+
+        // close window
+        if (input.wasPressed(Keys.ESCAPE)){
+            Window.close();
+        }
+
+        BACKGROUND_IMAGE.draw(Window.getWidth()/2.0, Window.getHeight()/2.0);
+
+    }
+}
